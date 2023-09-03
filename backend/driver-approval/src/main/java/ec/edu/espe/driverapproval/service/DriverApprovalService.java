f27a7fdb2981278160c14e5bd4e267f00a544146
Kevin-Chuquimarca <santi.kevin0936@gmail.com>
2023-09-02 19:45:59 -0500

    OC-6 add API versing in Driver Approval Service

--- backend/driver-approval/src/main/java/ec/edu/espe/driverapproval/service/DriverApprovalService.java
+++ backend/driver-approval/src/main/java/ec/edu/espe/driverapproval/service/DriverApprovalService.java
@@ -19,7 +19,8 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class DriverApprovalService {
     private final DriverRequestRepository driverRequestRepository;
-    public final RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
+
     public boolean approveDriverRequest(Integer id, String message) throws FileNotFoundException {
         Optional<DriverRequestEntity> driverRequestEntity = driverRequestRepository.findById(id);
         if (driverRequestEntity.isPresent()) {
@@ -27,7 +28,7 @@ public class DriverApprovalService {
             dre.setStateDr("A");
             dre.setMessageDr(message);
             DriverTripServiceDTO driver = new DriverTripServiceDTO(dre.getCodUserDr(), 0L, dre.getPlateCarDr(), dre.getPhotoCarDr(), dre.getCodUserDr());
-            if (changeUserRoleToDriver(dre.getCodUserDr()) && createDriverInTripService(driver) && saveDriverCarPhoto(dre.getPlateCarDr(), dre.getPhotoCarDr())){
+            if (changeUserRoleToDriver(dre.getCodUserDr()) && createDriverInTripService(driver) && saveDriverCarPhoto(dre.getPlateCarDr(), dre.getPhotoCarDr())) {
                 driverRequestRepository.save(dre);
                 return true;
             }
@@ -53,7 +54,7 @@ public class DriverApprovalService {
         return response.getStatusCode().is2xxSuccessful();
     }
 
-    public boolean createDriverInTripService(DriverTripServiceDTO driver){
+    public boolean createDriverInTripService(DriverTripServiceDTO driver) {
         String url = "http://localhost:8082/driver";
         ResponseEntity<DriverTripServiceDTO> response = restTemplate.postForEntity(url, driver, DriverTripServiceDTO.class);
         return response.getStatusCode().is2xxSuccessful();
@@ -61,7 +62,7 @@ public class DriverApprovalService {
 
     public boolean saveDriverCarPhoto(String plateCar, String photoCar) throws FileNotFoundException {
         String url = "http://localhost:8082/driver/img";
-        Resource resource = ImageUtil.loadImageAsResource("img/cars/"+photoCar);
+        Resource resource = ImageUtil.loadImageAsResource("img/cars/" + photoCar);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
         MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
